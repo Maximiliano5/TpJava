381d009c8882cb9b61c702cc86b52abba165aea0
Maximiliano5 <maxiromano98@gmial.com>
2023-04-21 23:28:53 -0300

    cree la clase imagen

--- Proyecto/src/proyecto/Imagen.java
+++ Proyecto/src/proyecto/Imagen.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyecto;
 
-/**
- *
- * @author Pc
- */
-public class Imagen {
+public class Imagen extends Publicacion implements Filtrable{
+    private int Resolucion;
+    private int Alto;
+    private int Ancho;
+    private Boolean Filtro;
+
+    //constructor
+    public Imagen(int Resolucion, int Alto, int Ancho, Boolean Filtro) {
+        this.Resolucion = Resolucion;
+        this.Alto = Alto;
+        this.Ancho = Ancho;
+        this.Filtro = Filtro;
+    } 
+    //metodo
+    @Override
+    public void Filtro() {
+        //hay que redefinir el metodo
+    }
     
 }
