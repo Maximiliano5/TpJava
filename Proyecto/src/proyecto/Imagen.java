232c70c38cdd97d4c2c344fd2cf8df4c3296e85a
Maximiliano5 <maxiromano98@gmial.com>
2023-04-22 18:40:49 -0300

    actualiza constructor Imagen

--- Proyecto/src/proyecto/Imagen.java
+++ Proyecto/src/proyecto/Imagen.java
@@ -1,5 +1,7 @@
 package proyecto;
 
+import java.util.ArrayList;
+
 public class Imagen extends Publicacion implements Filtrable{
     private int Resolucion;
     private int Alto;
@@ -7,16 +9,17 @@ public class Imagen extends Publicacion implements Filtrable{
     private Boolean Filtro;
 
     //constructor
-    public Imagen(int Resolucion, int Alto, int Ancho, Boolean Filtro) {
+    public Imagen(int Resolucion, int Alto, int Ancho, Boolean Filtro, String n, int f, int cant, ArrayList e, ArrayList com) {    
+        super(n, f, cant, e, com);
         this.Resolucion = Resolucion;
         this.Alto = Alto;
         this.Ancho = Ancho;
         this.Filtro = Filtro;
-    } 
-    //metodo
+    }
+
+    //metodo de la interfaz
     @Override
     public void Filtro() {
         //hay que redefinir el metodo
-    }
-    
+    }  
 }
