7eb651125135d4ee191c591fb2f8bfee9e1c7c02
josefinagarriz <131292319+josefinagarriz@users.noreply.github.com>
2023-04-22 13:59:44 -0300

    Update Audio.java

--- Proyecto/src/proyecto/Audio.java
+++ Proyecto/src/proyecto/Audio.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto;
 
-/**
- *
- * @author Pc
- */
-public class Audio {
-    
+public class Audio extends Publicacion implements durable{
+    private int duracion; //segundos
+    private int velbits; //Mbit/s
+    private boolean avanzar; //Sí o no
+    private boolean renaudar; //Sí o no
+    private boolean detener; //Sí o no
+
+    //constructor
+    public Audio(int d, int v, boolean a, boolean det){
+        duracion=d;
+        velbits=v;
+        avanzar=a;
+        detener=det;
+    }
+
+    //Metodos
+    public int obtDuracion(){
+        return duracion;
+    }
+    public int obtVelbits(){
+        return velbits;
+    }
+    public boolean obtAvanzar(){
+        return avanzar;
+    }
+    public boolean obtRenaudar(){
+        return renaudar;
+    }
+    public boolean obtDetener(){
+        return detener;
+    }
+    public void avanzar(){}
+    public void renaudar(){}
+    public void pausar(){}
+
 }
