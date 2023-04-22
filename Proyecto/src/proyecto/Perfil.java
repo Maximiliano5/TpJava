420acabfd505d1e8f992e6653a726436ab0ad0f3
Maximiliano5 <maxiromano98@gmial.com>
2023-04-21 21:52:34 -0300

    creo mapaAlbum y atributo apellido

--- Proyecto/src/proyecto/Perfil.java
+++ Proyecto/src/proyecto/Perfil.java
@@ -3,16 +3,21 @@ import java.util.TreeMap;
 
 public class Perfil {
     private String Nombre;
+    private String Apellido;
     private int Seguidores;
     private int Seguidos;
     private int Publicaciones;
-    private TreeMap<Integer, Publicacion> treeMap;  
+    private TreeMap<Integer, Publicacion> PubliMap;  
+    private TreeMap<Integer, Album> AlbuMap;
 
-    public Perfil(String Nombre, int Seguidores, int Seguidos, int Publicaciones, TreeMap<Integer, Publicacion> treeMap) {
+    public Perfil(String Nombre, String Apellido, int Seguidores, int Seguidos, int Publicaciones, TreeMap<Integer, Publicacion> PubliMap, TreeMap<Integer, Album> AlbuMap) {
         this.Nombre = Nombre;
+        this.Apellido = Apellido;
         this.Seguidores = Seguidores;
         this.Seguidos = Seguidos;
         this.Publicaciones = Publicaciones;
-        this.treeMap = treeMap;
+        this.PubliMap = PubliMap;
+        this.AlbuMap = AlbuMap;
     }
+    
 }
