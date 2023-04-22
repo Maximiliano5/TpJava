63c4876bf1cefb89208167883fc71bc14efa8bfa
josefinagarriz <131292319+josefinagarriz@users.noreply.github.com>
2023-04-22 13:58:31 -0300

    Update Publicacion.java

--- Proyecto/src/proyecto/Publicacion.java
+++ Proyecto/src/proyecto/Publicacion.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package proyecto;
 
-/**
- *
- * @author Pc
- */
+package proyecto;
+import java.util.*;
+
 public class Publicacion {
-    
+    private String nombre;
+    private int fecha; //aaaammdd
+    private int cantMG;
+    private ArrayList<String> etiquetas = new ArrayList<String>(50);
+    private ArrayList<String> comentarios = new ArrayList<String>(50);
+
+    //Constructor
+    public Publicacion(String n, int f, int cant, ArrayList e, ArrayList com){
+        nombre=n;
+        fecha=f;
+        cantMG=cant;
+        etiquetas=e;
+        comentarios=com;
+    }
+
+    //Metodos
+    public String obtNombre(){
+        return nombre;
+    }
+    public int obtFecha(){
+        return fecha;
+    }
+    public int obtCantMG(){
+        return cantMG;
+    }
+    public ArrayList obtEtiquetas(){
+        return etiquetas;
+    }
+    public ArrayList obtComentarios(){
+        return comentarios;
+    }
 }
+
