0103790b78d4226dcaaf3239631b4ba8a723d6fe
Sebavc2 <tpsebastianvaccaro@gmail.com>
2023-04-23 13:48:06 -0300

    clase album creada

--- Proyecto/src/proyecto/Album.java
+++ Proyecto/src/proyecto/Album.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto;
 
-/**
- *
- * @author Pc
- */
+import java.util.List;
+
+
+
 public class Album {
+    
+    private String nombreAlbum;//Criterio para separar publicaciones  
+    private  List<Publicacion> Lalbum;
+
+    public Album(String nombreAlbum, List<Publicacion> Lalbum) {
+        this.nombreAlbum = nombreAlbum;
+        this.Lalbum = Lalbum;
+    }
+
+    public String getNombreAlbum() {
+        return nombreAlbum;
+    }
+
+    public List<Publicacion> getLalbum() {
+        return Lalbum;
+    }
+
+    public void setNombreAlbum(String nombreAlbum) {
+        this.nombreAlbum = nombreAlbum;
+    }
+
+    public void setLalbum(List<Publicacion> Lalbum) {
+        this.Lalbum = Lalbum;
+    }
+
+    
+
+  
+    
+    
     
 }
