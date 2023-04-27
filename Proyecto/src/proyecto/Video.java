a3b5ecdf644ca96e440a9767510cc22013758c6b
josefinagarriz <131292319+josefinagarriz@users.noreply.github.com>
2023-04-27 08:53:21 -0300

    Update Video.java

--- Proyecto/src/proyecto/Video.java
+++ Proyecto/src/proyecto/Video.java
@@ -32,7 +32,7 @@ private  boolean reproduciendo;
     public void pausar(){
         reproduciendo=false;}
     
-    public void reanudar(){
+    public void renaudar(){
         reproduciendo=true;}
 
     //getters
