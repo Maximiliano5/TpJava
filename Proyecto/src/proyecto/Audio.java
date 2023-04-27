dd1928b9103cc88b54f01a0d14f4862d86bcf5c2
josefinagarriz <131292319+josefinagarriz@users.noreply.github.com>
2023-04-27 08:43:38 -0300

    Update Audio.java

--- Proyecto/src/proyecto/Audio.java
+++ Proyecto/src/proyecto/Audio.java
@@ -1,45 +1,36 @@
 
 package proyecto;
 
-import java.util.ArrayList;
-
-public class Audio extends Publicacion implements Durable{
+import java.util.*;
+public class Audio extends Publicacion implements durable{
     private int duracion; //segundos
     private int velbits; //Mbit/s
-    private boolean avanzar; //Sí o no
-    private boolean reanudar; //Sí o no
-    private boolean detener; //Sí o no
+    private  int tiempoact;
+    private  boolean reproduciendo;
 
     //constructor
-
-    public Audio(int duracion, int velbits, boolean avanzar, boolean reanudar, boolean detener, String n, int f, int cant, ArrayList e, ArrayList com) {
+    public Audio(int duracion, int velbits, int tiempoact, boolean reproduciendo, String n, int f, int cant, ArrayList e, ArrayList com) {
         super(n, f, cant, e, com);
         this.duracion = duracion;
         this.velbits = velbits;
-        this.avanzar = avanzar;
-        this.reanudar = reanudar;
-        this.detener = detener;
+        this.tiempoact = tiempoact;
+        this.reproduciendo = reproduciendo;
     }
-       
 
     //Metodos
-    public int obtDuracion(){
+    public int getDuracion(){
         return duracion;
     }
-    public int obtVelbits(){
+    public int getVelbits(){
         return velbits;
     }
-    public boolean obtAvanzar(){
-        return avanzar;
+    public int getTiempoact(){ 
+        return tiempoact; }
+    
+    public boolean getReproduciendo(){
+        return reproduciendo;
     }
-    public boolean obtRenaudar(){
-        return reanudar;
-    }
-    public boolean obtDetener(){
-        return detener;
-    }
-    public void avanzar(){}
-    public void reanudar(){}
-    public void pausar(){}
 
-}
+    public void avanzar(){}
+    public void renaudar(){}
+    public void pausar(){}
