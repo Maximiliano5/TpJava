d178944711f17e77a65c99f20e71495ed4af581b
josefinagarriz <131292319+josefinagarriz@users.noreply.github.com>
2023-05-15 15:33:58 -0300

    Update Durable.java

--- Proyecto/src/Logica/Durable.java
+++ Proyecto/src/Logica/Durable.java
@@ -1,7 +1,7 @@
 package Logica;
 
 public interface Durable {
-    public void avanzar();
-    public void reanudar();
-    public void pausar();
+    public void avanzar(int seg); //en segundos
+    public void renaudar(int seg); //en segundos
+    public void pausar(boolean res);
 }
