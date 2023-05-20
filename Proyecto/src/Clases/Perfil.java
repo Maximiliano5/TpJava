48ed6c5a9ece0974010ae5604358ca550867525d
Maximiliano <maxiromano98@gmail.com>
2023-05-20 18:00:45 -0300

    Seba Update Perfil

--- Proyecto/src/Clases/Perfil.java
+++ Proyecto/src/Clases/Perfil.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Perfil {
     private String Nombre;
@@ -89,31 +91,76 @@ public class Perfil {
         for(Publicacion auxp:pubList)
         {
             if(auxp instanceof Imagen)
-                imgList.add(auxp);
+                imgList.add((Imagen)auxp);
             else if(auxp instanceof Audio)
-                audList.add(auxp);
+                audList.add((Audio)auxp);
             else
-                vidList.add(auxp);
+                vidList.add((Video)auxp);
         }
     }
-    public void reportes(ArrayList<Imagen> imgList,ArrayList<Video> vidList, ArrayList<Audio> audList, int cantVideos, float promMGVideos,int cantAudios, float PromMGAudios,int cantImg,float PromMGImg)//agregar metodo que calcule el promedio pasandole lista de publicacoines y cant
+     private static float calcularPromedioLikes(List<? extends Publicacion> listaPublicaciones,int cantidadPubs) {
+        if (listaPublicaciones.isEmpty()) {
+            return 0;
+        }
+
+        int totalLikes = 0;
+        for (Publicacion publicacion : listaPublicaciones) {
+            totalLikes += publicacion.getCantMG();
+        }
+
+        return (float) totalLikes / cantidadPubs;
+    }
+     private static void escribirPublicaciones(BufferedWriter writer, List<? extends Publicacion> listaPublicaciones)
+        throws IOException {
+    if (listaPublicaciones.isEmpty()) {
+        writer.write("No hay publicaciones\n");
+    } else {
+        for (int i = 0; i < listaPublicaciones.size(); i++) {
+            writer.write("Publicación " + (i + 1) + ": " + listaPublicaciones.get(i).getNombre()+"\n");
+        }
+        writer.write("\n");
+    }
+}
+     private static String generarReportePubs(String nombreArchivo, List<Video> listaVideos, List<Audio> listaAudios,
+        List<Imagen> listaImagenes,int cantVideos, float promedioLikesVideos,int cantAudios, float promedioLikesAudios,int cantImagenes, float promedioLikesImagenes) {
+     cantVideos = listaVideos.size();
+     cantAudios = listaAudios.size();
+     cantImagenes = listaImagenes.size();
+     promedioLikesVideos=calcularPromedioLikes(listaVideos,cantVideos);
+     promedioLikesAudios=calcularPromedioLikes(listaAudios,cantAudios);
+     promedioLikesImagenes=calcularPromedioLikes(listaImagenes,cantImagenes);
+    StringBuilder reporte = new StringBuilder();
+
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(nombreArchivo))) {
+        writer.write("Reporte de Publicaciones\n\n");
+
+        writer.write("Videos:\n");
+        escribirPublicaciones(writer, listaVideos);
+        writer.write("Cantidad de videos: " + cantVideos + "\n");
+        writer.write("Promedio de likes en videos: " + promedioLikesVideos + "\n\n");
+
+        writer.write("Audios:\n");
+        escribirPublicaciones(writer, listaAudios);
+        writer.write("Cantidad de audios: " + cantAudios + "\n");
+        writer.write("Promedio de likes en audios: " + promedioLikesAudios + "\n\n");
+
+        writer.write("Imágenes:\n");
+        escribirPublicaciones(writer, listaImagenes);
+        writer.write("Cantidad de imágenes: " + cantImagenes + "\n");
+        writer.write("Promedio de likes en imágenes: " + promedioLikesImagenes + "\n");
+
+        reporte.append("Se generó el reporte en el archivo: ").append(nombreArchivo);
+    } catch (IOException e) {
+        reporte.append("Error al generar el reporte: ").append(e.getMessage());
+    }
+
+    return reporte.toString();
+}
+    public void reportes(ArrayList<Imagen> imgList,ArrayList<Video> vidList, ArrayList<Audio> audList, int cantVideos, float promMGVideos,int cantAudios, float promMGAudios,int cantImg,float promMGImg)//agregar metodo que calcule el promedio pasandole lista de publicaciones y cant
     {
-        filtraPubli(ArrayList<Imagen> imgList,ArrayList<Video> vidList, ArrayList<Audio> audList);
-        cantVideos=vidList.size();
-        cantAudios=audList.size();
-        cantImg=imgList.size();
-        float sumamg;
-        sumamg=0;
-        for(Imagen auxImg:imgList){
-                suma+=auxImg.getCantMG();}
-        promMGImg=suma/cantImg;
-        suma=0;
-        for(Audio auxAud:audList){
-                suma+=auxAud.getCantMG();}
-        promMGAudios=suma/cantAudios;
-        suma=0;
-        for(Video auxVid:vidList){
-            suma+=auxVid.getCantMG();}
-        promMGVideos=suma/cantVideos;
+        filtraPubli(imgList,vidList, audList);
+        generarReportePubs("reporte.txt",vidList,audList,imgList,cantVideos,promMGVideos,cantAudios,promMGAudios,cantImg,promMGImg);
+        
+       
     }
 }
